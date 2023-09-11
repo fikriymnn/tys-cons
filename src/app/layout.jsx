a0eb7a48f0935f20eb6cb453c80ea2c7b42cb20b
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2023-09-11 16:41:59 +0700

    Update layout.jsx
    Add component Navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import NavbarWithCTAButton from '@/components/NavbarWithCTAButton';
 
 import { Inter } from 'next/font/google'
 
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavbarWithCTAButton />
+        {children}
+      </body>
     </html>
   )
 }
