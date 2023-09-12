5cfe41f90f7ee633240efbeab48243c8ea1debdd
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-09-12 15:15:42 +0700

    Update ArticleCard.jsx
    Added anchor tag

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 function ArticleCard() {
     return (
-        <div className='bg-white shadow-xl'>
-            <div className='h-36 bg-blue-700'>
+        <a href="/articles/article">
+            <div className='bg-white shadow-xl hover:translate-y-[-10px]'>
+                <div className='h-36 bg-blue-700'>
+
+                </div>
+                <div className='p-3 '>
+                    <h1 className='font-semibold text-gray-900  '>Lorem Ipsum is simply dummy text of the printing and</h1>
+                    <h2>1 December 2022</h2>
+                </div>
 
             </div>
-            <div className='p-3 '>
-                <h1 className='font-semibold text-gray-900  '>Lorem Ipsum is simply dummy text of the printing and</h1>
-                <h2>1 December 2022</h2>
-            </div>
-
-        </div>
+        </a>
     )
 }
 
