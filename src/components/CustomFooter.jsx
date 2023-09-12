b1ffd64fe6cf23f42dac43345ae46ced3d9634db
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2023-09-12 17:06:30 +0700

    Create CustomFooter.jsx
    - Add Component Footer.jsx

---
+++ src/components/CustomFooter.jsx
@@ -0,0 +1,14 @@
+'use client';
+import React, { } from 'react'
+
+const CustomFooter = () => {
+    return (
+        <div className=' w-full bg-teal-900'>
+            <div className=' grid grid-cols-3'>
+
+            </div>
+        </div>
+    )
+}
+
+export default CustomFooter
