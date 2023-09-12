26c7647be9001f95f9d246f8f2b8c58fdb671ef6
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-09-12 15:15:04 +0700

    Update EventCard.jsx
    Added anchor tag

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 function EventCard() {
     return (
-        <div className='bg-white shadow-xl'>
-            <div className='h-36 bg-blue-700'>
+        <a href="/events/event">
+            <div className='bg-white shadow-xl hover:translate-y-[-10px]'>
+                <div className='h-36 bg-blue-700'>
+
+                </div>
+                <div className='p-3 '>
+                    <h1 className='font-medium text-xl text-gray-900  '>New Event</h1>
+                    <h2>4 December 2022</h2>
+                </div>
 
             </div>
-            <div className='p-3 '>
-                <h1 className='font-medium text-xl text-gray-900  '>New Event</h1>
-                <h2>4 December 2022</h2>
-            </div>
-
-        </div>
+        </a>
     )
 }
 
