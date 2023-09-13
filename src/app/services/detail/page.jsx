390c35e7aad82f19e5b90f2a42d57f3e415d7528
TionoviarID <Thenoviar@gmail.com>
2023-09-13 17:01:46 +0700

    update: -add officeAdministrationPage -add factory licenses page -add detail services -edit app services page -add trademark png - add construction certifications page

---
+++ src/app/services/detail/page.jsx
@@ -0,0 +1,115 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import { Label, Radio } from 'flowbite-react';
+
+function detailServices() {
+    return (
+        <div className='bg-gray-200 p-5'>
+            <div className='flex pb-5 gap-1'><p>Services </p> <p>&gt;</p><p className='text-blue-600'>公司商标注册 Company Trademark Registration</p></div>
+            <div className='bg-white'>
+                <div className="relative p-5 pt-10">
+                    <p>Tue Jan 03 2023</p>
+                    <div className='flex gap-1 mt-5'>
+                        <p className='text-sm'>Basic Establishment Services</p>
+                        <p className='text-sm'>&gt;</p>
+                        <p className='text-sm '>Company Registration</p>
+                    </div>
+                    <div className='flex mb-5'>
+                        <div className='bg-gray-100 w-[400px] h-[350px] mt-2'>
+
+                            <Image
+                                src="/assets/images/trademark.jpg"
+                                alt=''
+                                width={350}
+                                height={350}
+                            />
+                        </div>
+                        <div className='ps-10'>
+                            <p className='text-xl pb-4'>公司商标注册 Company Trademark Registration</p>
+                            <div className='flex pb-4'>
+                                <p className='text-blue-600 text-xl'>3500 - 3000 </p>
+                                <p className=''>元</p>
+
+                            </div>
+                            <p className='pb-4'>Options:</p>
+                            <fieldset
+                                className="flex gap-3 pb-4"
+                                id="radio"
+                            >
+
+                                <div className="flex items-center gap-2   border-gray-200  border p-3">
+                                    <Radio
+                                        id="foreign"
+                                        name="countries"
+                                        value="foreign"
+                                    />
+                                    <Label htmlFor="foreign">
+                                        外国公司 Foreign Company
+                                    </Label>
+                                </div>
+                                <div className="flex items-center gap-2 border-gray-200  border p-3">
+                                    <Radio
+                                        id="indonesia"
+                                        name="countries"
+                                        value="indonesia"
+                                    />
+                                    <Label htmlFor="indonesia">
+
+                                        印尼公司 Indonesia Company
+                                    </Label>
+                                </div>
+
+                            </fieldset>
+                        </div>
+                    </div>
+                    <div className='h-[2px] w-full bg-gray-300 '></div>
+                    <div className=' bg-blue-600  w-40 mb-2'>
+                        <p className='text-center text-white text-base font-semibold p-3'>Basic Information</p>
+                    </div>
+                    <p>1. The classification standard of trademarks is a common standard all over the world, and there are 45 categories</p>
+                    <p>2. A trademark is usually composed of a combination of words, graphics, English and numbers</p>
+                    <p>3. It takes two years to formally process a trademark, and you can start using it after obtaining the review approval. It takes about 2-3 weeks from the application to the approval letter</p>
+                    <div className='h-[2px] w-full bg-gray-300 '></div>
+                    <div className=' bg-blue-600  w-80 mb-2'>
+                        <p className='text-center text-white text-base font-semibold p-2'>Registration Time and Flow Process</p>
+                    </div>
+                    <p className='mb-2'>1. Registration Time: 12-24 months</p>
+                    <p>2. Flow Process:</p>
+                    <p>1&#41; Step 1: Trade Name Approval &#40;2 working days&#41;</p>
+                    <p>3) Step 3: PNBP payment (5 working days)</p>
+                    <p>4) Step 4: Apply for trademark registration (2 working days)</p>
+                    <p>5) Step Five: Formal Review (14 working days)</p>
+
+                    <p>6) Step 6: Announcement period (7 months)</p>
+                    <p>7) Step 7: Substantive Examination</p>
+                    <p>
+
+                        8) Step 8: Issuance a Trademark Certificate</p>
+                    <div className='h-[2px] w-full bg-gray-300 '></div>
+                    <div className=' bg-blue-600  w-80 mb-2'>
+                        <p className='text-center text-white text-base font-semibold p-2'>Required Documents</p>
+                    </div>
+                    <p>1. Trademark’s Logo</p>
+                    <div className='h-[2px] w-full bg-gray-300 '></div>
+                    <div className=' bg-blue-600  w-80 mb-2'>
+                        <p className='text-center text-white text-base font-semibold p-2'>Finished Registration Docoments</p>
+                    </div>
+                    <p>1. Trademark’s Logo</p>
+                    <div className='h-[2px] w-full bg-gray-300 '></div>
+                    <div className=' bg-blue-600  w-80 mb-2'>
+                        <p className='text-center text-white text-base font-semibold p-2'>Trademark Categories/Class</p>
+                    </div>
+                    <p>1. Trademark’s Logo</p>
+                    <div className='h-[2px] w-full bg-gray-300 '></div>
+                    <div className=' bg-blue-600  w-80 mb-2'>
+                        <p className='text-center text-white text-base font-semibold p-2'>Precautions</p>
+                    </div>
+                    <p>1. Trademark’s Logo</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default detailServices
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -6,96 +6,29 @@ import { Tabs } from 'flowbite-react';
 import NavbarWithCTAButton from '@/components/NavbarWithCTAButton';
 import VisaRegistrationPage from '@/components/ServicesSub/BasicEstablishmentServices/VisaRegistrationPage'
 import TrademarkPage from '@/components/ServicesSub/BasicEstablishmentServices/TrademarkPage'
-
+import OfficeAdministrationPage from '@/components/ServicesSub/BasicEstablishmentServices/officeAdministrationPage'
+import ConstructionCertificationsPage from '@/components/ServicesSub/BasicEstablishmentServices/ConstructionCertificationsPage'
+import FactoryLicensesPage from '@/components/ServicesSub/BasicEstablishmentServices/FactoryLicensesPage'
 function Events() {
     const components = [
         <CompanyRegistrationPage />,
 
 
     ]
-    const [comp, setComp] = useState(2);
+    const [comp, setComp] = useState(0);
     return (
         <div className='bg-gray-200 p-5'>
-            <div className='flex pb-5'><p>Services</p> <p className='text-blue-600'>/ Basic</p></div>
+            <div className='flex pb-5'><p>Services </p> <p className='text-blue-600'>/ Basic</p></div>
             <div className='bg-white'>
                 <div className="relative p-5 pt-10">
 
-                    <Tabs.Group
-                        aria-label="Tabs with underline"
-                        style="underline"
-
-                    >
-                        <Tabs.Item
-                            active
-                            onClick={() => setComp(0)}
-                            title="Company Registration"
-                        >
-
-                        </Tabs.Item>
-                        <Tabs.Item
-                            onClick={() => setComp(1)}
-                            title="Visa Registration"
-                        >
-                            <div onClick={() => setComp(1)}>
-
-                            </div>
-                        </Tabs.Item>
-                        <Tabs.Item
-                            onClick={() => setComp(2)}
-                            title="Trademark"
-                        >
-                            <div onClick={() => setComp(2)}>
-
-                            </div>
-                        </Tabs.Item>
-                        <Tabs.Item
-
-                            title="Office Administration"
-                        >
-                            <p>
-                                This is
-                                <span className="font-medium text-gray-800 dark:text-white">
-                                    Contacts tab's associated content
-                                </span>
-                                .
-                                Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-                                control the content visibility and styling.
-                            </p>
-                        </Tabs.Item>
-                        <Tabs.Item
-
-                            title="Construction Certification"
-                        >
-                            <p>
-                                This is
-                                <span className="font-medium text-gray-800 dark:text-white">
-                                    Contacts tab's associated content
-                                </span>
-                                .
-                                Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-                                control the content visibility and styling.
-                            </p>
-                        </Tabs.Item>
-                        <Tabs.Item
-
-                            title="Factory Licences"
-                        >
-                            <p>
-                                This is
-                                <span className="font-medium text-gray-800 dark:text-white">
-                                    Contacts tab's associated content
-                                </span>
-                                .
-                                Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-                                control the content visibility and styling.
-                            </p>
-                        </Tabs.Item>
-
-                    </Tabs.Group>
                     <div className='flex gap-9'>
                         <button onClick={() => setComp(0)}>Company Registration</button>
                         <button onClick={() => setComp(1)}>Visa Registration</button>
                         <button onClick={() => setComp(2)}>Trademark</button>
+                        <button onClick={() => setComp(3)}>OfficeAdministration</button>
+                        <button onClick={() => setComp(4)}>Construction Certifications</button>
+                        <button onClick={() => setComp(5)}>Factory Licenses</button>
                     </div>
                 </div>
                 {
@@ -123,7 +56,30 @@ function Events() {
 
 
                         </div>
-                    </> : comp == 3 ?
+                    </> : comp == 3 ? <>
+                        <p>Ini Office Administration</p>
+                        <div className='grid grid-cols-5 gap-5 px-5 pb-5'>
+                            <OfficeAdministrationPage />
+
+
+
+                        </div>
+                    </> : comp == 4 ? <>
+                        <p>Ini Construction Certifications</p>
+                        <div className='grid grid-cols-5 gap-5 px-5 pb-5'>
+                            <ConstructionCertificationsPage />
+
+
+
+                        </div>
+                    </> : comp == 5 ? <>
+                        <p>Ini Factory Licenses</p>
+                        <div className='grid grid-cols-5 gap-5 px-5 pb-5'>
+                            <FactoryLicensesPage />
+
+
+
+                        </div> </> : comp == 6 ?
 
                         components[0] : false
                 }
