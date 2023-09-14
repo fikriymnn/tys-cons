55c195cf1dfc9de21774b095047c086d94489b69
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2023-09-14 16:33:03 +0700

    Update page.jsx
    Update responsive to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,20 +7,20 @@ import Image from 'next/image'
 
 const About = () => {
     return (
-        <div className=' px-16 py-24'>
+        <div className=' px-5 md:px-16 pt-24 '>
             <p className='text-blue-800 text-3xl font-semibold pb-4 font-sans'>
                 About Us
             </p>
             <p className=' text-lg font-sans'>
                 TYS Consulting is a Business Consultant with main business in providing one-stop enterprise consultation services for enterprises or individuals who wants to establish business in Indonesia. Our team are equipped to communicate in Mandarin, English and Bahasa Indonesia with experiences on helping numerous customers in various sectors from establishment till ready to start business operation.
             </p>
-            <div className='grid grid-cols-2 py-10 gap-2'>
+            <div className='md:grid md:grid-cols-2 py-10 gap-2 '>
                 <div>
                     <Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d126941.19501629066!2d106.73087!3d-6.142476!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7fc6ab10259%3A0x8b5219b3d0af9b39!2sCitra%20Garden%20City%2C%20West%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1694419390459!5m2!1sen!2sid"
-                        width="600px"
-                        height="300px"
+                        width=""
+                        height=""
                         id=""
-                        className=""
+                        className="w-full h-80 pr-4"
                         display="block"
                         position="" />
                     <div className='flex gap-4'>
