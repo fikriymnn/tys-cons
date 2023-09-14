aeb747db98c21771ab90a197415ad79d471bbfb7
TionoviarID <Thenoviar@gmail.com>
2023-09-14 16:13:46 +0700

    update: add page

---
+++ src/components/ServicesSub/TalentRecruitmentHR/ManagementCandidate.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CompanyRegistration from '../../CompanyRegistrationCard'
+import CompanyRegistrationCard from '../../CompanyRegistrationCard'
+function ManagementCandidate() {
+    return (
+        <>
+
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+
+        </>
+    )
+}
+
+export default ManagementCandidate
