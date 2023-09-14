af2e0ed1cae817c65b1f351231c1d26af0c89596
TionoviarID <Thenoviar@gmail.com>
2023-09-14 14:29:51 +0700

    update: add alcohol and cigarette components

---
+++ src/components/ServicesSub/ProductCertificationsServices/AlcoholAndCigarette.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CompanyRegistration from '../../CompanyRegistrationCard'
+import CompanyRegistrationCard from '../../CompanyRegistrationCard'
+function
+    AlcoholAndCigarette() {
+    return (
+        <>
+
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+        </>
+    )
+}
+
+export default AlcoholAndCigarette
