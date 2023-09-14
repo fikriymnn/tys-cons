439b8dc571102159558a2079ea575d9b6ab3befa
TionoviarID <Thenoviar@gmail.com>
2023-09-14 14:26:03 +0700

    update: add

--- src/app/services/productCertifications/page.jsx
+++ src/app/services/productCertifications/page.jsx
@@ -13,6 +13,9 @@ import BPOMFoodDrug from '../../../components/ServicesSub/ProductCertificationsS
 import ISOManagementSystem from '../../../components/ServicesSub/ProductCertificationsServices/ISOManagementSystem';
 import SNINationalStandard from '@/components/ServicesSub/ProductCertificationsServices/SNINationalStandard'
 import MedicalAndHygiene from '@/components/ServicesSub/ProductCertificationsServices/MedicalAndHygiene'
+import POSTELTelecommunication from '@/components/ServicesSub/ProductCertificationsServices/POSTELTelecommunication'
+import AlcoholAndCigarette from '@/components/ServicesSub/ProductCertificationsServices/AlcoholAndCigarette'
+import OtherCer from '@/components/ServicesSub/ProductCertificationsServices/OtherCer'
 
 function ProductCertifications() {
     const components = [
@@ -23,17 +26,18 @@ function ProductCertifications() {
     const [comp, setComp] = useState(0);
     return (
         <div className='bg-gray-200 p-5'>
-            <div className='flex pb-5'><p>Services </p> <p className='text-blue-600 ml-1'>/ Basic Establishment Services</p></div>
+            <div className='flex pb-5'><p>Services </p> <p className='text-blue-600 ml-1'>/ Product Certifications</p></div>
             <div className='bg-white'>
                 <div className="p-5 pt-3">
                     <div className='overflow-auto w-full'>
-                        <div className='flex gap-9 border-b md:w-full sm:w-full w-[700px]  md:h-full sm:h-full h-16 '>
+                        <div className='flex gap-9 border-b md:w-full sm:w-[1000px] w-[1100px]  md:h-full sm:h-full h-16 '>
                             <button onClick={() => setComp(0)} className={`${comp == 0 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>BPOM Food & Drug</button>
                             <button onClick={() => setComp(1)} className={`${comp == 1 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>ISO Management System</button>
                             <button onClick={() => setComp(2)} className={`${comp == 2 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>SNI National Standard</button>
                             <button onClick={() => setComp(3)} className={`${comp == 3 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>Medical and Hygiene</button>
-                            <button onClick={() => setComp(4)} className={`${comp == 4 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>Construction Certifications</button>
-                            <button onClick={() => setComp(5)} className={`${comp == 5 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>Factory Licenses</button>
+                            <button onClick={() => setComp(4)} className={`${comp == 4 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>POSTEL Telecommunication</button>
+                            <button onClick={() => setComp(5)} className={`${comp == 5 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>Alcohol and Cigarette</button>
+                            <button onClick={() => setComp(6)} className={`${comp == 6 ? "text-blue-600 border-b pb-2 border-blue-600" : "text-black border-0"} md:text-base sm:text-sm text-sm`}>Other Certification</button>
                         </div>
                     </div>
 
@@ -73,7 +77,7 @@ function ProductCertifications() {
                     </> : comp == 4 ? <>
                         <p>Ini Construction Certifications</p>
                         <div className='grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1   gap-5 px-5 pb-5'>
-                            <ConstructionCertificationsPage />
+                            <POSTELTelecommunication />
 
 
 
@@ -81,13 +85,20 @@ function ProductCertifications() {
                     </> : comp == 5 ? <>
                         <p>Ini Factory Licenses</p>
                         <div className='grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1   gap-5 px-5 pb-5'>
-                            <FactoryLicensesPage />
+                            <AlcoholAndCigarette />
 
 
 
                         </div> </> : comp == 6 ?
 
-                        components[0] : false
+                        <>
+                            <p>Ini Factory Licenses</p>
+                            <div className='grid md:grid-cols-5 sm:grid-cols-3 grid-cols-1   gap-5 px-5 pb-5'>
+                                <OtherCer />
+
+
+
+                            </div> </> : false
                 }
             </div>
         </div>
