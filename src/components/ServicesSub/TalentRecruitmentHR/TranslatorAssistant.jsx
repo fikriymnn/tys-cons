72ccba3e3dd0b146d7a0d9df37b1af4955612596
TionoviarID <Thenoviar@gmail.com>
2023-09-14 16:15:11 +0700

    update: add page

---
+++ src/components/ServicesSub/TalentRecruitmentHR/TranslatorAssistant.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CompanyRegistration from '../../CompanyRegistrationCard'
+import CompanyRegistrationCard from '../../CompanyRegistrationCard'
+function TranslatorAssistant() {
+    return (
+        <>
+
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+
+        </>
+    )
+}
+
+export default TranslatorAssistant
