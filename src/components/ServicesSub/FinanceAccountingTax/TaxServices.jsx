ef4a3ee4c29a8ae13ced8bbfcede798f074d4798
TionoviarID <Thenoviar@gmail.com>
2023-09-14 16:12:20 +0700

    update: add page

---
+++ src/components/ServicesSub/FinanceAccountingTax/TaxServices.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import CompanyRegistration from '../../CompanyRegistrationCard'
+import CompanyRegistrationCard from '../../CompanyRegistrationCard'
+function
+    TaxServices() {
+    return (
+        <>
+
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+            <CompanyRegistrationCard />
+
+        </>
+    )
+}
+
+export default TaxServices
