c5d8a9c7d0e11c7412495c6935419b07653a3166
Claudeuss <133555372+Claudeuss@users.noreply.github.com>
2023-09-14 16:34:42 +0700

    Update NavbarWithCTAButton.jsx
    Update :
    - Responsive

--- src/components/NavbarWithCTAButton.jsx
+++ src/components/NavbarWithCTAButton.jsx
@@ -8,11 +8,11 @@ export default function NavbarWithCTAButton() {
     const Dropdown = () => {
         return (
             <div className=' bg-white  absolute mt-10 z-10 shadow-lg '>
-                <div className=' py-1 flex gap-1 hover p-2 hover:bg-slate-100'>
+                <div className=' py-1 flex gap-1 hover p-2 hover:bg-blue-300'>
                     <Image src={'/assets/images/united-states.png'} width={40} height={10} />
                     <p className=' my-auto'>English</p>
                 </div>
-                <div className=' py-1 flex gap-1 p-2 hover:bg-slate-100'>
+                <div className=' py-1 flex gap-1 p-2 hover:bg-blue-300'>
                     <Image src={'/assets/images/china.png'} width={40} height={10} />
                     <p className=' my-auto'>Chinese</p>
                 </div>
@@ -21,11 +21,11 @@ export default function NavbarWithCTAButton() {
     }
     return (
         <>
-            <Navbar className=' fixed justify-beetwen w-full'
+            <Navbar className=' fixed justify-beetwen w-full z-20 shadow-md'
                 fluid
-                rounded
+
             >
-                <Navbar.Brand href="https://flowbite-react.com">
+                <Navbar.Brand href="/">
                     {/* <img
                         alt="Flowbite React Logo"
                         className="mr-3 h-6 sm:h-9"
@@ -34,52 +34,49 @@ export default function NavbarWithCTAButton() {
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
                         Flowbite React
                     </span> */}
-                    <Image src={'/assets/images/tys-logo-blue.png'} width={200} height={125} alt='' />
+                    <img src="/assets/images/tys-logo-blue.png" alt="" className=' md:w-52 md:h-14 w-36 h-10' />
                 </Navbar.Brand>
-                <div className="flex md:order-2 gap-4">
-                    <Button className=' bg-white rounded-none h-10 hover:bg-black'>
-                        <p className=' text-black text-base'>Log in</p>
-                    </Button>
-                    <Button className=' bg-black rounded-none h-10 hover:bg-white'>
-                        <p className=' text-white text-base'>Sign Up</p>
-                    </Button>
-                    <div className=' my-auto w-auto cursor-pointer flex px-2 gap-2 mr-10 ' onClick={() => setOpen(!open)}>
+                <div className="flex md:order-2 md:gap-4 gap-1">
+
+                    <div className=' my-auto w-auto cursor-pointer flex px-2 gap-2 md:mr-10 ' onClick={() => setOpen(!open)}>
                         <Image src={'/assets/images/united-states.png'} width={40} height={10} />
                         <p className=' text-base my-auto'>EN</p>
                         {
                             open && <Dropdown />
-
-
                         }
 
                     </div>
                     <Navbar.Toggle />
                 </div>
 
-                <Navbar.Collapse>
+                <Navbar.Collapse className=' my-auto'>
                     <Navbar.Link
                         active
                         href="#"
                     >
-                        <p>
-                            Home
-                        </p>
+                        <span className=' my-auto text-base'>Home</span>
                     </Navbar.Link>
                     <Navbar.Link href="about">
-                        About
+                        <span className=' my-auto text-base'>About</span>
                     </Navbar.Link>
                     <Navbar.Link href="#">
-                        Services
+                        <span className=' text-base'>Services</span>
                     </Navbar.Link>
                     <Navbar.Link href="#">
-                        Articles
+                        <span className=' text-base'>Articles</span>
                     </Navbar.Link>
                     <Navbar.Link href="#">
-                        Events
+                        <span className=' text-base'>Events</span>
                     </Navbar.Link>
                     <Navbar.Link href="#">
-                        Policies & Regulations
+                        <span className='text-base'>Policies & Regulations</span>
                     </Navbar.Link>
+                    <Button className=' bg-white rounded-sm md:h-10 my-auto'>
+                        <span className=' text-black md:text-base'>Log in</span>
+                    </Button>
+                    <Button className=' bg-black rounded-sm md:h-10 my-auto'>
+                        <span className=' text-white md:text-base'>Sign Up</span>
+                    </Button>
                 </Navbar.Collapse>
             </Navbar>
         </>
