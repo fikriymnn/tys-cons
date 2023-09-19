b3dbf9c029813ab3553f3b4607581118744b8f2d
kitacumantemen1 <mochfadhil219@gmail.com>
2023-09-19 16:47:17 +0700

    Update MainCard.jsx
    update responsive on MainCard.jsx

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import { Card } from 'flowbite-react';
 function MainCard({ icon, title, text1, text2, text3, text4, text5, text6, text7, clickText1, clickText2, clickText3, clickText4, clickText5, clickText6, clickText7 }) {
      return (
-          <Card className='group block rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500  group-hover:text-white hover:underline'>
+          <Card className='group block rounded-lg p-6 md:bg-white bg-blue-600 ring-1 ring-slate-900/5 shadow-lg space-y-3 md:hover:bg-sky-500 hover:ring-sky-500  group-hover:text-white hover:underline'>
 
                <div className="flex items-baseline text-gray-900  first-linexport hover:/assets/images/icontools3.png 
                      {mainCard}">
                     <img className='w-10 group-hover:stroke-white m-1' src={icon} alt="" />
 
-                    <span className="ml-1 text-xl font-normal my-auto  text-black  group-hover:text-white">
+                    <span className="border-b-2 border-white md:border-none ml-1 text-xl font-normal my-auto  text-black  group-hover:text-white">
                          {title}
                     </span>
 
