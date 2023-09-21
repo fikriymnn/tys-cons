e372ba10fc2403e9a2c579c069b4079ceb52aa58
TionoviarId <Thenoviar@gmail.com>
2023-09-21 17:18:57 +0700

    make dinamis data

--- src/components/ServicesSub/TalentRecruitmentHR/MarketingSales.jsx
+++ src/components/ServicesSub/TalentRecruitmentHR/MarketingSales.jsx
@@ -2,18 +2,56 @@ import React from 'react'
 import CompanyRegistration from '../../CompanyRegistrationCard'
 import CompanyRegistrationCard from '../../CompanyRegistrationCard'
 function MarketingSales() {
+    const Card1 = (
+
+        <CompanyRegistrationCard src={"/foto.jpg"}
+            title={"Company Registration Basic Regulation"}
+            price={
+                "3000-2000"
+            } />
+    );
+    const Card2 = (
+
+        <CompanyRegistrationCard src={"/foto.jpg"}
+            title={"dakjnlk Basic Regulation"}
+            price={
+                "2000-2000"
+            } />
+    );
+    const Card3 = (
+
+        <CompanyRegistrationCard src={"/foto.jpg"}
+            title={"Company Registration Basic Regulation Company Registration Basic RegulationCompany Registration Basic RegulationCompany Registration Basic Regulation"}
+            price={
+                "2000-2000"
+            } />
+    );
+    const Card4 = (
+
+        <CompanyRegistrationCard src={"/foto.jpg"}
+            title={"Company Registration Basic Regulation"}
+            price={
+                "2000-2000"
+            } />
+    );
+
+    const Card5 = (
+
+        <CompanyRegistrationCard src={"/foto.jpg"}
+            title={"Company Registration Basic Regulation"}
+            price={
+                "2000-2000"
+            } />
+    );
+
+    const CompanyReg = [
+        Card1, Card2, Card3, Card4, Card5
+    ]
     return (
         <>
 
-            <CompanyRegistrationCard />
-            <CompanyRegistrationCard />
-            <CompanyRegistrationCard />
-            <CompanyRegistrationCard />
-            <CompanyRegistrationCard />
-            <CompanyRegistrationCard />
-            <CompanyRegistrationCard />
-            <CompanyRegistrationCard />
 
+            {CompanyReg}
         </>
     )
 }
