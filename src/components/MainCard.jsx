8c2726ceb94fe2e3f206d8f5d390c53295b2b345
kitacumantemen1 <mochfadhil219@gmail.com>
2023-09-21 16:53:29 +0700

    Update MainCard.jsx
    Update:
    - Delete ring on MainCard.jsx
    - Update <Card> to <div>

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -3,75 +3,76 @@ import React from 'react'
 import { Card } from 'flowbite-react';
 function MainCard({ icon, title, text1, text2, text3, text4, text5, text6, text7, clickText1, clickText2, clickText3, clickText4, clickText5, clickText6, clickText7 }) {
      return (
-          <Card className='group block rounded-lg md:p-6 md:bg-white bg-blue-600 ring-1 ring-slate-900/5 shadow-lg space-y-3 md:hover:bg-sky-500 hover:ring-sky-500  group-hover:text-white'>
+          <div className='group block md:rounded-lg rounded-none md:p-6 p-0 md:bg-white bg-blue-600 shadow-lg  md:hover:bg-sky-500 group-hover:text-white'>
 
-               <div className="flex items-baseline text-gray-900  first-linexport hover:/assets/images/icontools3.png 
+               <div className="flex items-baseline text-gray-900  first-linexport hover:/assets/images/icontools3.png  
                      {mainCard}">
-                    <img className='w-10 group-hover:stroke-white m-1' src={icon} alt="" />
+                    <img className='w-10  m-1 pl-3' src={icon} alt="" />
 
-                    <span className="border-b-2 border-white md:border-none ml-1 text-xl font-normal my-auto  text-black  group-hover:text-white md:text-white">
+                    <span className=" md:border-none ml-1 text-xl font-normal my-auto  text-black  group-hover:text-white pl-3 ">
                          {title}
                     </span>
 
                </div>
-               <hr className='' />
+               {title ? <hr className='' /> : <></>}
 
                <ul className="my-7 space-y-5">
                     <a href={clickText1}>
 
-                         <li className="flex space-x-3">
-                              <span className="text-base font-normal leading-tight text-black group-hover:text-white">
+                         <li className="flex space-x-3 ">
+                              <span className="text-base font-normal leading-tight text-black group-hover:text-white pl-3">
 
 
                                    {text1}
 
                               </span>
                          </li>
+                         {text1 ? <hr className='' /> : <></>}
                     </a>
-                    <hr className='' />
 
-                    <li className="flex space-x-3">
 
-                         <span className="text-base font-normal leading-tight text-black group-hover:text-white">
+                    <li className="flex space-x-3 ">
+
+                         <span className="text-base font-normal leading-tight text-black group-hover:text-white pl-3">
                               {text2}
                          </span>
                     </li>
-                    <hr className='' />
-                    <li className="flex space-x-3">
+                    {text2 ? <hr className='' /> : <></>}
+                    <li className="flex space-x-3 ">
 
-                         <span className="text-base font-normal leading-tight text-black group-hover:text-white">
+                         <span className="text-base font-normal leading-tight text-black group-hover:text-white pl-3">
                               {text3}
                          </span>
                     </li>
-                    <hr className='' />
-                    <li className="flex space-x-3">
+                    {text3 ? <hr className='' /> : <></>}
+                    <li className="flex space-x-3 ">
 
-                         <span className="text-base font-normal leading-tight text-black group-hover:text-white">
+                         <span className="text-base font-normal leading-tight text-black group-hover:text-white pl-3">
                               {text4}
                          </span>
                     </li>
-                    <hr className='' />
-                    <li className="flex space-x-3">
+                    {text4 ? <hr className='' /> : <></>}
+                    <li className="flex space-x-3 ">
 
-                         <span className="text-base font-normal leading-tight text-black group-hover:text-white">
+                         <span className="text-base font-normal leading-tight text-black group-hover:text-white pl-3">
                               {text5}
                          </span>
                     </li>
-                    <hr className='' />
+                    {text5 ? <hr className='' /> : <></>}
                     <li className="flex space-x-3">
 
                          <span className="text-base font-normal leading-tight text-black  group-hover:text-white">
                               {text6}
                          </span>
                     </li>
-                    <hr className='' />
+                    {text6 ? <hr className='' /> : <></>}
                     <li className="flex space-x-3">
 
-                         <span className="text-base font-normal leading-tight text-black group-hover:text-white">
+                         <span className="text-base font-normal leading-tight text-black group-hover:text-white pl-3">
                               {text7}
                          </span>
                     </li>
-                    <hr className='' />
+                    {text7 ? <hr className='' /> : <></>}
 
 
                </ul>
@@ -79,7 +80,7 @@ function MainCard({ icon, title, text1, text2, text3, text4, text5, text6, text7
 
 
 
-          </Card>
+          </div>
      )
 }
 
