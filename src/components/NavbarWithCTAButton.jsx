2bd12f3f57aab838d12ca049de1c4fc2d02990ce
TionoviarId <Thenoviar@gmail.com>
2023-09-26 16:15:22 +0700

    fixed navbar styles

--- src/components/NavbarWithCTAButton.jsx
+++ src/components/NavbarWithCTAButton.jsx
@@ -12,6 +12,7 @@ export default function NavbarWithCTAButton({ height }) {
 
     const [navbar, SetNavbar] = useState(false);
     const ChangeBG = () => {
+
         if (window.scrollY <= height) {
             SetNavbar(true)
         } else {
@@ -19,7 +20,11 @@ export default function NavbarWithCTAButton({ height }) {
         }
 
     }
-    window.addEventListener('scroll', ChangeBG)
+    if (typeof window !== "undefined") {
+        // browser code
+        window.addEventListener('scroll', ChangeBG)
+    }
+
 
     const DropdownServices = () => {
         return (
@@ -59,25 +64,25 @@ export default function NavbarWithCTAButton({ height }) {
     }
     return (
         <>
-            <Navbar className={navbar ? 'bg-white opacity-40 fixed w-full md:py-5 sm:py-5 py-7 z-10 shadow-md top-0 ' : ' fixed w-full md:py-5 sm:py-5 py-7 z-10 shadow-md top-0 bg-white'}
+            <Navbar className={navbar ? 'bg-[#d9d9d943] fixed w-full md:py-5 sm:py-5 py-7 z-30 shadow-md top-0 ' : ' fixed w-full md:py-5 sm:py-5 py-7 z-30 shadow-md top-0 bg-white'}
                 fluid
 
             >
-                <Navbar.Brand href="/" className='z-40  '>
+                <Navbar.Brand href="/" className='z-40  opacity-100'>
+                    {navbar ? <img src="/assets/images/tys-logo.png" alt="" className=' md:w-52 md:h-12 sm:w-36 sm:h-10 w-44 opacity-100' /> : <img src="/assets/images/tys-logo-blue.png" alt="" className=' md:w-52 md:h-12 sm:w-36 sm:h-10 w-44 opacity-100' />}
 
-                    <img src="/assets/images/tys-logo-blue.png" alt="" className=' md:w-52 md:h-12 sm:w-36 sm:h-10 w-44 opacity-100' />
                 </Navbar.Brand>
                 <div className="flex md:order-3 gap-1 z-40 ">
 
                     <div className=' my-auto w-auto cursor-pointer flex px-2 gap-2 md:mr-3 ' >
                         <Image src={'/assets/images/united-states.png'} width={40} height={10} />
-                        <p className=' text-base my-auto'>EN</p>
+                        <p className={navbar ? 'text-white text-base my-auto' : '  text-black my-auto text-base  '}>EN</p>
 
 
                     </div>
                     <div className=' my-auto w-auto cursor-pointer flex px-2 gap-2 md:mr-3 ' >
                         <Image src={'/assets/images/China.png'} width={40} height={10} />
-                        <p className=' text-base my-auto'>语言</p>
+                        <p className={navbar ? 'text-white text-base my-auto' : '  text-black my-auto text-base  '}>语言</p>
 
 
                     </div>
@@ -89,18 +94,18 @@ export default function NavbarWithCTAButton({ height }) {
 
                             href="/"
                         >
-                            <span className=' my-auto md:text-sm '>Home</span>
+                            <span className={navbar ? 'text-white' : '  text-black my-auto md:text-sm  '}>Home</span>
                         </Navbar.Link>
                     </div>
                     <div className=' my-auto'>
                         <Navbar.Link href="/about">
-                            <span className=' my-auto md:text-sm '>About Us</span>
+                            <span className={navbar ? 'text-white' : '  text-black my-auto md:text-sm  '}>About Us</span>
                         </Navbar.Link>
                     </div>
                     <div className=' my-auto hover:text-blue-500' onClick={() => setOpen2(!open2)}>
                         <Navbar.Link href="#" className=' flex gap-1'>
-                            <span className=' md:text-sm '>Services</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" className=' text-slate-700 w-4 h-4 my-auto ' viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M112 184l144 144 144-144" /></svg>
+                            <span className={navbar ? 'text-white' : '  text-black my-auto md:text-sm  '}>Services</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className={navbar ? 'text-white w-4 h-4 my-auto ' : ' text-slate-700 w-4 h-4 my-auto '} viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="48" d="M112 184l144 144 144-144" /></svg>
                             {
                                 open2 && <DropdownServices />
                             }
@@ -108,17 +113,17 @@ export default function NavbarWithCTAButton({ height }) {
                     </div>
                     <div className=' my-auto'>
                         <Navbar.Link href="/articles">
-                            <span className='md:text-sm '>Articles</span>
+                            <span className={navbar ? 'text-white' : '  text-black my-auto md:text-sm  '}>Articles</span>
                         </Navbar.Link>
                     </div>
                     <div className=' my-auto'>
                         <Navbar.Link href="/events">
-                            <span className=' md:text-sm '>Events</span>
+                            <span className={navbar ? 'text-white' : '  text-black my-auto md:text-sm  '}>Events</span>
                         </Navbar.Link>
                     </div>
                     <div className=' my-auto'>
                         <Navbar.Link href="/policies">
-                            <span className='md:text-sm '>Policies & Regulations</span>
+                            <span className={navbar ? 'text-white' : '  text-black my-auto md:text-sm  '}>Policies & Regulations</span>
                         </Navbar.Link>
                     </div>
 
