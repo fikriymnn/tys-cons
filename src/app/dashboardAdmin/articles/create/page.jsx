7c0ad7c685b001add57379331171b7637d4e5be7
acepW <acepwahyu001@gmail.com>
2023-09-28 21:18:59 +0700

    update articles

--- src/app/dashboardAdmin/articles/create/page.jsx
+++ src/app/dashboardAdmin/articles/create/page.jsx
@@ -1,6 +1,24 @@
 "use client";
 import React from "react";
 import { useState, useEffect } from "react";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  where,
+  query,
+  deleteDoc,
+  updateDoc,
+  doc,
+  Firestore,
+} from "firebase/firestore";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { db, storage } from "../../../../../firebase/page";
 
 function CreateArticle() {
   const [isAlert, setIsAlert] = useState(false);
@@ -10,6 +28,80 @@ function CreateArticle() {
   const closeAlert = () => {
     setIsAlert(false);
   };
+
+  const [titleIng, setTitleIng] = useState("");
+  const [titleChi, setTitleChi] = useState("");
+  const [headTextIng, setHeadTextIng] = useState("");
+  const [headTextChi, setHeadTextChi] = useState("");
+  const [contentIng, setContentIng] = useState("");
+  const [contentChi, setcontentChi] = useState("");
+
+  const [downloadURL, setDownloadURL] = useState("");
+
+  // progress
+  const [percent, setPercent] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const handleUpload = async (filess) => {
+    const files = filess;
+    try {
+      setLoading(true);
+      const storageRef = ref(storage, `/articles/${files.name}`);
+
+      // progress can be paused and resumed. It also exposes progress updates.
+      // Receives the storage reference and the file to upload.
+      const uploadTask = uploadBytesResumable(storageRef, files);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const percent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+
+          // update progress
+          setPercent(percent);
+        },
+        (err) => console.log(err),
+        () => {
+          // download url
+
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            console.log(url);
+            setDownloadURL(url);
+            setLoading(false);
+          });
+        }
+      );
+    } catch (error) {
+      alert(error);
+      setLoading(false);
+    }
+  };
+
+  const addData = async (e) => {
+    e.preventDefault();
+    var today = new Date();
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    const docRef = await addDoc(collection(db, "articles"), {
+      titleEnglish: titleIng,
+      titleChinese: titleChi,
+      headTextEnglish: headTextIng,
+      headTextChinese: headTextChi,
+      img: downloadURL,
+      date: date,
+      contentEnglish: contentIng,
+      ContentChinese: contentChi,
+    });
+
+    alert("success");
+  };
+
   return (
     <>
       {isAlert && (
@@ -57,7 +149,10 @@ function CreateArticle() {
               <p>Image</p>
             </div>
             <div className=" w-10/12 p-3">
-              <input type="file" />
+              <input
+                type="file"
+                onChange={(event) => handleUpload(event.target.files[0])}
+              />
             </div>
           </div>
           <div className=" flex py-1 px-20 ">
@@ -72,6 +167,7 @@ function CreateArticle() {
             </div>
             <div className=" w-10/12 p-3">
               <input
+                onChange={(e) => setTitleIng(e.target.value)}
                 type="text"
                 placeholder="Insert Title"
                 color=" bg-transparent"
@@ -85,6 +181,7 @@ function CreateArticle() {
             </div>
             <div className=" w-10/12 p-3">
               <input
+                onChange={(e) => setTitleChi(e.target.value)}
                 type="text"
                 placeholder="Insert Title"
                 color=" bg-transparent"
@@ -105,6 +202,7 @@ function CreateArticle() {
             </div>
             <div className=" w-10/12 p-3">
               <input
+                onChange={(e) => setHeadTextIng(e.target.value)}
                 type="text"
                 placeholder="Insert Price"
                 color=" bg-transparent"
@@ -118,6 +216,7 @@ function CreateArticle() {
             </div>
             <div className=" w-10/12 p-3">
               <input
+                onChange={(e) => setHeadTextChi(e.target.value)}
                 type="text"
                 placeholder="Insert Price"
                 color=" bg-transparent"
@@ -142,6 +241,7 @@ function CreateArticle() {
             </div>
             <div className=" w-10/12 p-3">
               <textarea
+                onChange={(e) => setContentIng(e.target.value)}
                 name=""
                 id=""
                 cols="20"
@@ -159,6 +259,7 @@ function CreateArticle() {
             </div>
             <div className=" w-10/12 p-3">
               <textarea
+                onChange={(e) => setcontentChi(e.target.value)}
                 name=""
                 id=""
                 cols="20"
@@ -172,7 +273,10 @@ function CreateArticle() {
           </div>
           <div className="mx-20">
             <div className=" flex items-end justify-end mx-3">
-              <button className="p-3 px-7  rounded-lg mb-5 text-white bg-green-400">
+              <button
+                onClick={(e) => addData(e)}
+                className="p-3 px-7  rounded-lg mb-5 text-white bg-green-400"
+              >
                 Create New Article
               </button>
             </div>
