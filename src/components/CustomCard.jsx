b36fcb393d17c940b0fd417f29d5bfe28021d52e
TionoviarId <thenoviar@gmail.com>
2023-10-02 15:50:28 +0700

    update

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Card } from 'flowbite-react';
 function CustomCard({ text, isi, isi2 }) {
      return (
-          <Card className='transform hover:-translate-y-6 my-5 '
+          <Card className='transform hover:-translate-y-6 my-1 h-full'
                imgAlt="Meaningful alt text for an image that is not purely decorative"
                imgSrc="/assets/images/bgtys.png"
           >
-               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+               <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
 
                     {text}
 
