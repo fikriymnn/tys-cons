9ee9f1817d6efdcab4e05f8badb8d4b1f67507f2
Daffa Fauzan <dafazanlbs@gmail.com>
2023-10-04 12:59:09 +0700

    added svg icons for navigation

--- src/components/admin/navigation.jsx
+++ src/components/admin/navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Image from "next/image";
 
 function Navigation() {
   const [bar, setBar] = useState(1);
@@ -7,15 +8,14 @@ function Navigation() {
     setBar((prevBar) => (prevBar === 1 ? 0 : 1));
   };
   return (
-    <div className="z-40">
+    <div className="z-40 ">
       <div
-        className={`${
-          bar == 1
-            ? " w-[200px] duration-100 ease-in-out"
-            : " w-[80px] duration-100 ease-in-out"
-        } h-screen shadow-md flex-col`}
+        className={`${bar == 1
+          ? " w-[200px] duration-100 ease-in-out"
+          : " w-[80px] duration-100 ease-in-out"
+          } h-screen shadow-md flex-col`}
       >
-        <div className="w-full h-[80px] mt-10 shadow-md  ">
+        <div className="w-full h-[80px] mb-5 shadow-md  ">
           <button className="w-full h-full" onClick={toggleBar}>
             {bar === 0 ? (
               <img
@@ -34,86 +34,195 @@ function Navigation() {
         </div>
         <a href="/dashboardAdmin/home">
           <button
-            className={`${
-              bar == 1
-                ? "ms-5 justify-start items-center"
-                : " justify-center items-center"
-            } w-full h-10 flex `}
+            className={`${bar == 1
+              ? "ms-5 justify-start items-center"
+              : " justify-center items-center"
+              } w-full h-10 flex `}
           >
-            {bar === 0 ? <p>H</p> : <p className="">Homepage</p>}
+            {bar === 0 ?
+
+              <Image
+                src="homepage.svg"
+                alt="My Image"
+                width={30}
+                height={30}
+              />
+              : <div className="flex gap-2">
+                <Image
+                  src="homepage.svg"
+                  alt="My Image"
+                  width={30}
+                  height={30}
+                />
+                <div className="flex items-center justify-center font-semibold">
+                  <p>Homepage</p>
+                </div>
+              </div>
+            }
           </button>
         </a>
         <a href="/dashboardAdmin/about">
           <button
-            className={`${
-              bar == 1
-                ? "ms-5 justify-start items-center"
-                : " justify-center items-center"
-            } w-full h-10 flex `}
+            className={`${bar == 1
+              ? "ms-5 justify-start items-center"
+              : " justify-center items-center"
+              } w-full h-10 flex `}
           >
-            {bar === 0 ? <p>A</p> : <p>About</p>}
+            {bar === 0 ?
+              <Image
+                src="information-sign.svg"
+                alt="My Image"
+                width={30}
+                height={30}
+              />
+              : <div className="flex gap-2">
+                <Image
+                  src="information-sign.svg"
+                  alt="My Image"
+                  width={30}
+                  height={30}
+                />
+                <div className="flex items-center justify-center font-semibold">
+                  <p>About</p>
+                </div>
+              </div>}
           </button>
         </a>
         <a href="/dashboardAdmin/services">
           <button
-            className={`${
-              bar == 1
-                ? "ms-5 justify-start items-center"
-                : " justify-center items-center"
-            } w-full h-10 flex `}
+            className={`${bar == 1
+              ? "ms-5 justify-start items-center"
+              : " justify-center items-center"
+              } w-full h-10 flex `}
           >
             {bar === 0 ? (
-              <p>S</p>
+              <Image
+                src="services.svg"
+                alt="My Image"
+                width={30}
+                height={30}
+              />
             ) : (
-              <>
-                <p>Services</p>
-                {}
-              </>
+              <div className="flex gap-2">
+                <Image
+                  src="services.svg"
+                  alt="My Image"
+                  width={30}
+                  height={30}
+                />
+                <div className="flex items-center justify-center font-semibold">
+                  <p>Services</p>
+                </div>
+              </div>
             )}
           </button>
         </a>
         <a href="/dashboardAdmin/articles">
           <button
-            className={`${
-              bar == 1
-                ? "ms-5 justify-start items-center"
-                : " justify-center items-center"
-            } w-full h-10 flex `}
+            className={`${bar == 1
+              ? "ms-5 justify-start items-center"
+              : " justify-center items-center"
+              } w-full h-10 flex `}
           >
-            {bar === 0 ? <p>A</p> : <p>Articles</p>}
+            {bar === 0 ?
+              <Image
+                src="newspaper.svg"
+                alt="My Image"
+                width={30}
+                height={30}
+              />
+              : <div className="flex gap-2">
+                <Image
+                  src="newspaper.svg"
+                  alt="My Image"
+                  width={30}
+                  height={30}
+                />
+                <div className="flex items-center justify-center font-semibold">
+                  <p>Articles</p>
+                </div>
+              </div>}
           </button>
         </a>
         <a href="/dashboardAdmin/events">
           <button
-            className={`${
-              bar == 1
-                ? "ms-5 justify-start items-center"
-                : " justify-center items-center"
-            } w-full h-10 flex `}
+            className={`${bar == 1
+              ? "ms-5 justify-start items-center"
+              : " justify-center items-center"
+              } w-full h-10 flex `}
           >
-            {bar === 0 ? <p>E</p> : <p>Event</p>}
+            {bar === 0 ?
+              <Image
+                src="megaphone.svg"
+                alt="My Image"
+                width={30}
+                height={30}
+              />
+              : <div className="flex gap-2">
+                <Image
+                  src="megaphone.svg"
+                  alt="My Image"
+                  width={30}
+                  height={30}
+                />
+                <div className="flex items-center justify-center font-semibold">
+                  <p>Events</p>
+                </div>
+              </div>}
           </button>
         </a>
         <a href="/dashboardAdmin/policies">
           <button
-            className={`${
-              bar == 1
-                ? "ms-5 justify-start items-center"
-                : " justify-center items-center"
-            } w-full h-10 flex `}
+            className={`${bar == 1
+              ? "ms-5 justify-start items-center"
+              : " justify-center items-center"
+              } w-full h-10 flex `}
           >
-            {bar === 0 ? <p>PR</p> : <p>Pol and Reg</p>}
+            {bar === 0 ?
+              <Image
+                src="exclamation-mark.svg"
+                alt="My Image"
+                width={30}
+                height={30}
+              />
+              : <div className="flex gap-2">
+                <Image
+                  src="exclamation-mark.svg"
+                  alt="My Image"
+                  width={30}
+                  height={30}
+                />
+                <div className="flex items-center justify-center font-semibold">
+                  <p>Pol & Reg</p>
+                </div>
+              </div>}
           </button>
         </a>
         <a href="/dashboardAdmin/clients">
           <button
-            className={`${
-              bar == 1
-                ? "ms-5 justify-start items-center"
-                : " justify-center items-center"
-            } w-full h-10 flex `}
+            className={`${bar == 1
+              ? "ms-5 justify-start items-center"
+              : " justify-center items-center"
+              } w-full h-10 flex `}
           >
-            {bar === 0 ? <p>C</p> : <p>Clients</p>}
+            {bar === 0 ?
+              <Image
+                src="checked.svg"
+                alt="My Image"
+                width={30}
+                height={30}
+              />
+              : <div className="flex gap-2">
+                <Image
+                  src="checked.svg"
+                  alt="My Image"
+                  width={30}
+                  height={30}
+                />
+                <div className="flex items-center justify-center font-semibold">
+                  <p>Clients</p>
+                </div>
+              </div>}
           </button>
         </a>
       </div>
