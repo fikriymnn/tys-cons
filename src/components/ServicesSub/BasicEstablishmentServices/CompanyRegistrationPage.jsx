de295eb48a229663272d13f0fae472c59ef27286
TionoviarId <thenoviar@gmail.com>
2023-10-09 15:05:12 +0700

    fix

--- src/components/ServicesSub/BasicEstablishmentServices/CompanyRegistrationPage.jsx
+++ src/components/ServicesSub/BasicEstablishmentServices/CompanyRegistrationPage.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import CompanyRegistration from '../../CompanyRegistrationCard'
 import CompanyRegistrationCard from '../../CompanyRegistrationCard'
-function CompanyRegistrationPage() {
+function CompanyRegistrationPage({ title, img, price, id, key }) {
 
 
     const Card1 = (
 
-        <CompanyRegistrationCard src={"/foto.jpg"}
-            title={"Company Registration Basic Regulation"}
+        <CompanyRegistrationCard src={img}
+            title={title}
             price={
-                "3000-2000"
+                price
             } />
     );
     const Card2 = (
@@ -43,9 +43,23 @@ function CompanyRegistrationPage() {
     ]
     return (
         <>
+            <a key={key} href="/services/detail">
 
 
-            {CompanyReg}
+                <div className='bg-white shadow-xl hover:translate-y-[-10px] duration-300 md:block sm:block grid grid-cols-2 md:h-72'>
+                    <div className=' bg-blue-700'>
+                        <img src={img} alt="" />
+                    </div>
+                    <div className='p-3 md:w-full sm:w-full w-11/12 md:h-20'>
+                        <h1 className='font-semibold text-gray-900  md:text-base sm:text-base text-sm mb-2 line-clamp-2 '>{title}</h1>
+                        <h2 className='md:text-base sm:text-sm text-sm text-blue-600'>{price}</h2>
+                    </div>
+
+
+
+
+                </div>
+            </a>
         </>
     )
 }
