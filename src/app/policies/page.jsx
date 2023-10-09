27e745cd7d4ec5b093fd61ae76b6c9e88f4302ea
TionoviarId <thenoviar@gmail.com>
2023-10-09 15:04:12 +0700

    remove footer

--- src/app/policies/page.jsx
+++ src/app/policies/page.jsx
@@ -1,13 +1,12 @@
 "use client";
 import { useState } from "react";
 import PoliciesDetail from "@/components/PoliciesDetail";
-import Image from "next/image";
-import CustomFooter from '@/components/CustomFooter';
-import NavbarWithCTAButton from '@/components/NavbarWithCTAButton';
+import Image from "next/image"; import NavbarWithCTAButton from "@/components/NavbarWithCTAButton";
+import CustomFooter from "@/components/CustomFooter";
 
 export default function Policies() {
-  const [dropdown, setDropdown] = useState(false);
-  const [selectedDetail, setSelectedDetail] = useState(0);
+  const [dropdown, setDropdown] = useState(true);
+  const [selectedDetail, setSelectedDetail] = useState(1);
 
   const defaultDetail = <PoliciesDetail />;
   const defaultDetail2 = <PoliciesDetail />;
@@ -107,139 +106,141 @@ export default function Policies() {
 
   return (
     <>
-
       <NavbarWithCTAButton />
-      <div className="md:flex md:justify-evenly sm:flex sm:justify-evenly grid grid-cols-1 justify-items-center  pt-24">
-        <div className="md:w-3/12 sm:w-4/12 w-10/12 mx-5 h-full bg-blue-600 ">
-          <div
-            className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${dropdown ? "bg-blue-700" : ""
-              }`}
-            onClick={() => {
-              setDropdown(!dropdown);
-            }}
-          >
-            <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm text-start w-full">
-              Foreign Company Registration
-            </p>
-            {dropdown ? (
-              <Image
-                src={"/dropdown.png"}
-                width={15}
-                height={15}
-                className="mr-5 rotate-180"
-              />
-            ) : (
-              <Image
-                src={"/dropdown.png"}
-                width={15}
-                height={15}
-                className="mr-5"
-              />
-            )}
-          </div>
-          {/* dropdown */}
-          {dropdown && (
-            <div className="border-y border-white">
-              <div
-                className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 1 ? "bg-blue-900" : ""
-                  }`}
-                onClick={() => {
-                  setSelectedDetail(1);
-                }}
-              >
-                <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
-                  Company Registration Basic Regulation
-                </p>
-              </div>
-              <div
-                className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 2 ? "bg-blue-900" : ""
-                  }`}
-                onClick={() => {
-                  setSelectedDetail(2);
-                }}
-              >
-                <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
-                  Trading Company Regulation
-                </p>
-              </div>
-              <div
-                className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 3 ? "bg-blue-900" : ""
-                  }`}
-                onClick={() => {
-                  setSelectedDetail(3);
-                }}
-              >
-                <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
-                  Representative Office Regulations
-                </p>
-              </div>
-              <div
-                className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 4 ? "bg-blue-900" : ""
-                  }`}
-                onClick={() => {
-                  setSelectedDetail(4);
-                }}
-              >
-                <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
-                  Construction Company Regulation
-                </p>
-              </div>
-              <div
-                className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 5 ? "bg-blue-900" : ""
-                  }`}
-                onClick={() => {
-                  setSelectedDetail(5);
-                }}
-              >
-                <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
-                  Factory Company Regulation
-                </p>
-              </div>
+      <div>
+        <div className="md:flex md:justify-evenly sm:flex sm:justify-evenly grid grid-cols-1 justify-items-center   pt-24">
+          <div className="md:w-3/12 sm:w-4/12 w-10/12 mx-5 h-full bg-blue-600 ">
+            <div
+              className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${dropdown ? "bg-blue-700" : ""
+                }`}
+              onClick={() => {
+                setDropdown(!dropdown);
+              }}
+            >
+              <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm text-start w-full">
+                Foreign Company Registration
+              </p>
+              {dropdown ? (
+                <Image
+                  src={"/dropdown.png"}
+                  width={15}
+                  height={15}
+                  className="mr-5 rotate-180"
+                />
+              ) : (
+                <Image
+                  src={"/dropdown.png"}
+                  width={15}
+                  height={15}
+                  className="mr-5"
+                />
+              )}
             </div>
-          )}
+            {/* dropdown */}
+            {dropdown && (
+              <div className="border-y border-white">
+                <div
+                  className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 1 ? "bg-blue-900" : ""
+                    }`}
+                  onClick={() => {
+                    setSelectedDetail(1);
+                  }}
+                >
+                  <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
+                    Company Registration Basic Regulation
+                  </p>
+                </div>
+                <div
+                  className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 2 ? "bg-blue-900" : ""
+                    }`}
+                  onClick={() => {
+                    setSelectedDetail(2);
+                  }}
+                >
+                  <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
+                    Trading Company Regulation
+                  </p>
+                </div>
+                <div
+                  className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 3 ? "bg-blue-900" : ""
+                    }`}
+                  onClick={() => {
+                    setSelectedDetail(3);
+                  }}
+                >
+                  <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
+                    Representative Office Regulations
+                  </p>
+                </div>
+                <div
+                  className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 4 ? "bg-blue-900" : ""
+                    }`}
+                  onClick={() => {
+                    setSelectedDetail(4);
+                  }}
+                >
+                  <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
+                    Construction Company Regulation
+                  </p>
+                </div>
+                <div
+                  className={`w-full h-[50px]  border-white flex items-center cursor-pointer ${selectedDetail == 5 ? "bg-blue-900" : ""
+                    }`}
+                  onClick={() => {
+                    setSelectedDetail(5);
+                  }}
+                >
+                  <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
+                    Factory Company Regulation
+                  </p>
+                </div>
+              </div>
+            )}
 
-          <div
-            className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${selectedDetail == 6 ? "bg-blue-900" : ""
-              }`}
-          >
-            <p
-              className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full"
+            <div
+              className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${selectedDetail == 6 ? "bg-blue-900" : ""
+                }`}
+            >
+              <p
+                className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full"
+                onClick={() => {
+                  setSelectedDetail(6);
+                }}
+              >
+                Tax Regulation
+              </p>
+            </div>
+            <div
+              className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${selectedDetail == 7 ? "bg-blue-900" : ""
+                }`}
+            >
+              <p
+                className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm w-full"
+                onClick={() => {
+                  setSelectedDetail(7);
+                }}
+              >
+                Labor Policy
+              </p>
+            </div>
+            <div
+              className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${selectedDetail == 8 ? "bg-blue-900" : ""
+                }`}
               onClick={() => {
-                setSelectedDetail(6);
+                setSelectedDetail(8);
               }}
             >
-              Tax Regulation
-            </p>
+              <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
+                Import Export Procedures & Policies
+              </p>
+            </div>
+
           </div>
-          <div
-            className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${selectedDetail == 7 ? "bg-blue-900" : ""
-              }`}
-          >
-            <p
-              className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm w-full"
-              onClick={() => {
-                setSelectedDetail(7);
-              }}
-            >
-              Labor Policy
-            </p>
-          </div>
-          <div
-            className={`w-full h-16  border-y border-white flex items-center cursor-pointer ${selectedDetail == 8 ? "bg-blue-900" : ""
-              }`}
-            onClick={() => {
-              setSelectedDetail(8);
-            }}
-          >
-            <p className="ml-5 mr-5 text-white md:text-[15px] sm:text-xs text-sm  w-full">
-              Import Export Procedures & Policies
-            </p>
-          </div>
+
+          {componentDetail[selectedDetail]}
+
         </div>
-
-        {componentDetail[selectedDetail]}
       </div>
-      <CustomFooter />
     </>
   );
 }
