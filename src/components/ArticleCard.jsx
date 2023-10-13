27eac6be9ec3d59b9679edecc901b50953466307
Dafazan <dafazanlbs@gmail.com>
2023-10-13 13:15:47 +0700

    update article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 function ArticleCard({ img, title, date, id }) {
   return (
     <a href={`/articles/article?id=${id}`}>
-      <div className="bg-white shadow-xl hover:translate-y-[-10px] transition-transform duration-50 ease-in-out grid grid-cols-2 md:grid-cols-1">
-        <div className="bg-blue-700 md:grid grid-cols-1 min-h-28 md:h-36 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${img})` }}>
+      <div className="bg-white shadow-xl md:hover:translate-y-[-10px] transition-transform duration-50 ease-in-out grid grid-cols-2 md:grid-cols-1">
+        <div className="bg-blue-700 md:grid grid-cols-1 h-28 md:h-36 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${img})` }}>
 
         </div>
         <div className="p-3 ">
