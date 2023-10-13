d9b144feb28dcd2325190c919c0a03461ccb4e2b
Dafazan <thenoviar@gmail.com>
2023-10-13 11:34:54 +0700

    tess

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -10,38 +10,8 @@ const LanguageContext = createContext();
 export function LanguageProvider({ children }) {
     const [languagee,setLanguagee]=useState("")
     
-    // useEffect(() => {
-    // getLang();
-    // }, [])
-
-    // function getLang(){
-    //     if (typeof window !== 'undefined') {
-    //         // Perform localStorage action
-    //         const lang=  sessionStorage.getItem('language');
-    //         setLanguagee(sessionStorage.getItem('language'));
-    //         console.log(languagee)
-    //       }
-    // }
-let data;
-    if (typeof window !== 'undefined') {
-        // console.log('You are on the browser')
-        // 👉️ can use localStorage here
-      
-         data=  sessionStorage.getItem('language');
-         
-      
-        
-       
-      } else {
-        // console.log('You are on the server');
-        
-
-      }
-     // const lang=  sessionStorage.getItem('language');
-
-    // console.log(data);
-  // Bahasa Inggris (default)
-  const [language, setLanguage] = useState(data||"en"); 
+    
+  const [language, setLanguage] = useState("en"); 
 
   const changeLanguage = (newLanguage) => {
     setLanguage(newLanguage);
