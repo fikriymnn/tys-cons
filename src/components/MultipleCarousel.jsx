c38685487b991d569b5820aeb07c99ba666323ab
Fikri Zaki Aditama, S.Si <fikriyomann@student.upi.edu>
2023-10-25 17:13:41 +0700

    hide arrow carousel client

--- src/components/MultipleCarousel.jsx
+++ src/components/MultipleCarousel.jsx
@@ -65,33 +65,37 @@ function MultipleCarousel() {
     },
   };
 
+  const [hide, setHide] = useState(false);
+
   return (
-    <Carousel
-      className="pb-16 mb-10 md:w-full"
-      responsive={responsive}
-      autoPlay={true}
-      infinite={"true"}
-      showDots="true"
-      autoPlaySpeed={1000}
-      removeArrowOnDeviceType={"desktop"}
-    >
-      {dataClient.map((data, i) => {
-        return (
-          <>
-            <div className="text-white flex justify-center items-center h-[80px]">
-              <Image
-                key={i}
-                className="md:w-[200px] w-32"
-                src={data.img}
-                width={200}
-                height={200}
-                alt=""
-              />
-            </div>
-          </>
-        );
-      })}
-    </Carousel>
+    <div onMouseEnter={() => setHide(true)} onMouseLeave={() => setHide(false)}>
+      <Carousel
+        className="pb-16 mb-10 md:w-full"
+        responsive={responsive}
+        autoPlay={true}
+        infinite={"true"}
+        showDots="true"
+        autoPlaySpeed={1000}
+        arrows={hide == true ? true : false}
+      >
+        {dataClient.map((data, i) => {
+          return (
+            <>
+              <div className="text-white flex justify-center items-center h-[80px]">
+                <Image
+                  key={i}
+                  className="md:w-[200px] w-32"
+                  src={data.img}
+                  width={200}
+                  height={200}
+                  alt=""
+                />
+              </div>
+            </>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 }
 
