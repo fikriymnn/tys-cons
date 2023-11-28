a93c95ed5f2c088a81181c2816bf0123e4700369
TionoviarId <Thenoviar@gmail.com>
2023-11-28 16:49:09 +0700

    menambah field loc and fee

--- src/app/dashboardAdmin/events/create/page.jsx
+++ src/app/dashboardAdmin/events/create/page.jsx
@@ -341,6 +341,29 @@ function CreateEvent() {
             </div>
             <div className=" flex py-1 px-20 ">
               <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
+                <p>Location</p>
+              </div>
+              <div className=" w-10/12 "></div>
+            </div>
+
+            {/* location */}
+            <div className=" flex py-1 px-20 ">
+              <div className=" w-2/12 text-end p-3 py-5">
+                <p>Loc :</p>
+              </div>
+              <div className=" w-10/12 p-3">
+                <input
+                  // onChange={(e) => setTimeFrom(e.target.value)}
+                  type="text"
+                  required
+                  placeholder="Insert Location"
+                  color=" bg-transparent"
+                  className=" rounded-lg w-full border-slate-300 "
+                />
+              </div>
+            </div>
+            <div className=" flex py-1 px-20 ">
+              <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
                 <p>Duration</p>
               </div>
               <div className=" w-10/12 "></div>
@@ -406,6 +429,44 @@ function CreateEvent() {
                   type="text"
                   required
                   placeholder="Insert Duration"
+                  color=" bg-transparent"
+                  className=" rounded-lg w-full border-slate-300 "
+                />
+              </div>
+            </div>
+
+            {/* Fee */}
+            <div className=" flex py-1 px-20 ">
+              <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
+                <p>Fee</p>
+              </div>
+              <div className=" w-10/12 "></div>
+            </div>
+            <div className=" flex py-1 px-20 ">
+              <div className=" w-2/12 text-end p-3 py-5">
+                <p>Rupiah :</p>
+              </div>
+              <div className=" w-10/12 p-3">
+                <input
+                  // onChange={(e) => setTimeFrom(e.target.value)}
+                  type="text"
+                  required
+                  placeholder="Insert fee"
+                  color=" bg-transparent"
+                  className=" rounded-lg w-full border-slate-300 "
+                />
+              </div>
+            </div>
+            <div className=" flex py-1 px-20 ">
+              <div className=" w-2/12 text-end p-3 py-5">
+                <p>Yuan :</p>
+              </div>
+              <div className=" w-10/12 p-3">
+                <input
+                  // onChange={(e) => setTimeFrom(e.target.value)}
+                  type="text"
+                  required
+                  placeholder="Insert fee"
                   color=" bg-transparent"
                   className=" rounded-lg w-full border-slate-300 "
                 />
--- src/app/dashboardAdmin/events/edit/page.jsx
+++ src/app/dashboardAdmin/events/edit/page.jsx
@@ -412,6 +412,28 @@ function EditEvent() {
           </div>
           <div className=" flex py-1 px-20 ">
             <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
+              <p>Location</p>
+            </div>
+            <div className=" w-10/12 "></div>
+          </div>
+          <div className=" flex py-1 px-20 ">
+            <div className=" w-2/12 text-end p-3 py-5">
+              <p>Location :</p>
+            </div>
+            <div className=" w-10/12 p-3">
+              <input
+                // onChange={(e) => setTimeFrom(e.target.value)}
+                type="text"
+                required
+                placeholder="Insert location"
+                color=" bg-transparent"
+                className=" rounded-lg w-full border-slate-300 "
+              />
+            </div>
+          </div>
+
+          <div className=" flex py-1 px-20 ">
+            <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
               <p>Duration</p>
             </div>
             <div className=" w-10/12 "></div>
@@ -477,6 +499,42 @@ function EditEvent() {
                 onChange={(e) => setTimeTo(e.target.value)}
                 type="text"
                 placeholder="Insert Duration"
+                color=" bg-transparent"
+                className=" rounded-lg w-full border-slate-300 "
+              />
+            </div>
+          </div>
+          <div className=" flex py-1 px-20 ">
+            <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
+              <p>Fee</p>
+            </div>
+            <div className=" w-10/12 "></div>
+          </div>
+          <div className=" flex py-1 px-20 ">
+            <div className=" w-2/12 text-end p-3 py-5">
+              <p>Rupiah :</p>
+            </div>
+            <div className=" w-10/12 p-3">
+              <input
+                // onChange={(e) => setTimeFrom(e.target.value)}
+                type="text"
+                required
+                placeholder="Insert fee"
+                color=" bg-transparent"
+                className=" rounded-lg w-full border-slate-300 "
+              />
+            </div>
+          </div>
+          <div className=" flex py-1 px-20 ">
+            <div className=" w-2/12 text-end p-3 py-5">
+              <p>Yuan :</p>
+            </div>
+            <div className=" w-10/12 p-3">
+              <input
+                // onChange={(e) => setTimeFrom(e.target.value)}
+                type="text"
+                required
+                placeholder="Insert fee"
                 color=" bg-transparent"
                 className=" rounded-lg w-full border-slate-300 "
               />
