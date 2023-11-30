8a29a6745a30452d040a380795923177ade31742
TionoviarId <Thenoviar@gmail.com>
2023-11-30 13:43:08 +0700

    navbar and homePage image

--- src/components/home/homePage.jsx
+++ src/components/home/homePage.jsx
@@ -30,7 +30,13 @@ function HomePage({
       </div>
       <div className="bg-black">
         <div className='flex items-center justify-center bg-left bg-cover bg-[url("/assets/images/bgtys.png")] text-white'>
-          <div className="w-full h-full flex items-center bakg py-12 p-5 lg:px-[10%] py-[20vh] lg:py-[30vh]">
+          <div
+            className={
+              language == "en"
+                ? "w-full h-full flex items-center bakg  p-5 lg:px-[10%] py-[20vh] lg:py-[30vh]  "
+                : "w-full h-full flex items-center bakg  p-5 lg:px-[10%] py-[30vh] lg:py-[38vh] "
+            }
+          >
             <div className="flex flex-col text-left lg:max-w-[50%]">
               <div className="font-semibold text-[1.75rem] lg:text-[2.5rem]">
                 {dataHeading.map((data, i) => {
