eeed2c59645d42ae435a0bae3ef3ae0b3afe81e9
TionoviarId <Thenoviar@gmail.com>
2023-12-21 16:19:20 +0700

    up

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  serverlessConfig: {
-    experimental: {
-      ssr: {
-        cache: "none",
-      },
-    },
-  },
 };
 
 module.exports = nextConfig;
--- src/app/services/productCertifications/page.jsx
+++ src/app/services/productCertifications/page.jsx
@@ -1,174 +1,186 @@
-import { collection, getDocs, where, query, orderBy } from "firebase/firestore";
-
+"use client";
+import {
+  collection,
+  getDocs,
+  where,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { useState, useEffect } from "react";
 import { db, storage, firebaseAnalytics } from "../../../../firebase/page";
 import ProductCertification from "@/components/service/productCertification";
 
-async function getDataProductBPOM() {
-  let data = [];
-  try {
-    const q = query(
-      collection(db, "service"),
-      where("service", "==", "Product Certifications"),
-      where("subService", "==", "BPOM Food and Drug"),
-      orderBy("createdAt", "desc")
-    );
+function ProductCertifications() {
+  const [DataProductBPOM, setDataProductBPOM] = useState([]);
+  const [DataProductISO, setDataProductISO] = useState([]);
+  const [DataProductSNI, setDataProductSNI] = useState([]);
+  const [DataProductMedical, setDataProductMedical] = useState([]);
+  const [dataProductPostel, setdataProductPostel] = useState([]);
+  const [DataProductAlcohol, setDataProductAlcohol] = useState([]);
+  const [DataProductOther, setDataProductOther] = useState([]);
 
-    const querySnapshot = await getDocs(q);
+  useEffect(() => {
+    getDataProductBPOM();
+    getDataProductISO();
+    getDataProductSNI();
+    getDataProductMedical();
+    getdataProductPostel();
+    getDataProductAlcohol();
+    getDataProductOther();
+  }, []);
 
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
+  async function getDataProductBPOM() {
+    try {
+      const q = query(
+        collection(db, "service"),
+        where("service", "==", "Product Certifications"),
+        where("subService", "==", "BPOM Food and Drug"),
+        orderBy("createdAt", "desc")
+      );
+
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setDataProductBPOM(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return data;
-}
-async function getDataProductISO() {
-  let data = [];
-  try {
-    const q = query(
-      collection(db, "service"),
-      where("service", "==", "Product Certifications"),
-      where("subService", "==", "ISO Management System"),
-      orderBy("createdAt", "desc")
-    );
+  async function getDataProductISO() {
+    try {
+      const q = query(
+        collection(db, "service"),
+        where("service", "==", "Product Certifications"),
+        where("subService", "==", "ISO Management System"),
+        orderBy("createdAt", "desc")
+      );
 
-    const querySnapshot = await getDocs(q);
-
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setDataProductISO(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return data;
-}
-async function getDataProductSNI() {
-  let data = [];
-  try {
-    const q = query(
-      collection(db, "service"),
-      where("service", "==", "Product Certifications"),
-      where("subService", "==", "SNI National Standard"),
-      orderBy("createdAt", "desc")
-    );
+  async function getDataProductSNI() {
+    try {
+      const q = query(
+        collection(db, "service"),
+        where("service", "==", "Product Certifications"),
+        where("subService", "==", "SNI National Standard"),
+        orderBy("createdAt", "desc")
+      );
 
-    const querySnapshot = await getDocs(q);
-
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setDataProductSNI(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return data;
-}
-async function getDataProductMedical() {
-  let data = [];
-  try {
-    const q = query(
-      collection(db, "service"),
-      where("service", "==", "Product Certifications"),
-      where("subService", "==", "Medical and Hygiene"),
-      orderBy("createdAt", "desc")
-    );
+  async function getDataProductMedical() {
+    try {
+      const q = query(
+        collection(db, "service"),
+        where("service", "==", "Product Certifications"),
+        where("subService", "==", "Medical and Hygiene"),
+        orderBy("createdAt", "desc")
+      );
 
-    const querySnapshot = await getDocs(q);
-
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setDataProductMedical(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return data;
-}
-async function getDataProductPostel() {
-  let data = [];
-  try {
-    const q = query(
-      collection(db, "service"),
-      where("service", "==", "Product Certifications"),
-      where("subService", "==", "POSTEL Telecommunication"),
-      orderBy("createdAt", "desc")
-    );
+  async function getdataProductPostel() {
+    try {
+      const q = query(
+        collection(db, "service"),
+        where("service", "==", "Product Certifications"),
+        where("subService", "==", "POSTEL Telecommunication"),
+        orderBy("createdAt", "desc")
+      );
 
-    const querySnapshot = await getDocs(q);
-
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setdataProductPostel(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return data;
-}
-async function getDataProductAlcohol() {
-  let data = [];
-  try {
-    const q = query(
-      collection(db, "service"),
-      where("service", "==", "Product Certifications"),
-      where("subService", "==", "Alcohol and Cigarette"),
-      orderBy("createdAt", "desc")
-    );
+  async function getDataProductAlcohol() {
+    try {
+      const q = query(
+        collection(db, "service"),
+        where("service", "==", "Product Certifications"),
+        where("subService", "==", "Alcohol and Cigarette"),
+        orderBy("createdAt", "desc")
+      );
 
-    const querySnapshot = await getDocs(q);
-
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setDataProductAlcohol(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return data;
-}
-async function getDataProductOther() {
-  let data = [];
-  try {
-    const q = query(
-      collection(db, "service"),
-      where("service", "==", "Product Certifications"),
-      where("subService", "==", "Other Certification"),
-      orderBy("createdAt", "desc")
-    );
+  async function getDataProductOther() {
+    try {
+      const q = query(
+        collection(db, "service"),
+        where("service", "==", "Product Certifications"),
+        where("subService", "==", "Other Certification"),
+        orderBy("createdAt", "desc")
+      );
 
-    const querySnapshot = await getDocs(q);
-
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setDataProductOther(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return data;
-}
-
-async function ProductCertifications() {
-  const dataProductBPOM = await getDataProductBPOM();
-  const dataProductISO = await getDataProductISO();
-  const dataProductSNI = await getDataProductSNI();
-  const dataProductMedical = await getDataProductMedical();
-  const dataProductPostel = await getDataProductPostel();
-  const dataProductAlcohol = await getDataProductAlcohol();
-  const dataProductOther = await getDataProductOther();
 
   return (
     <ProductCertification
-      dataProductBPOM={JSON.parse(JSON.stringify(dataProductBPOM))}
-      dataProductISO={JSON.parse(JSON.stringify(dataProductISO))}
-      dataProductSNI={JSON.parse(JSON.stringify(dataProductSNI))}
-      dataProductMedical={JSON.parse(JSON.stringify(dataProductMedical))}
+      dataProductBPOM={JSON.parse(JSON.stringify(DataProductBPOM))}
+      dataProductISO={JSON.parse(JSON.stringify(DataProductISO))}
+      dataProductSNI={JSON.parse(JSON.stringify(DataProductSNI))}
+      dataProductMedical={JSON.parse(JSON.stringify(DataProductMedical))}
       dataProductPostel={JSON.parse(JSON.stringify(dataProductPostel))}
-      dataProductAlcohol={JSON.parse(JSON.stringify(dataProductAlcohol))}
-      dataProductOther={JSON.parse(JSON.stringify(dataProductOther))}
+      dataProductAlcohol={JSON.parse(JSON.stringify(DataProductAlcohol))}
+      dataProductOther={JSON.parse(JSON.stringify(DataProductOther))}
     />
   );
 }
